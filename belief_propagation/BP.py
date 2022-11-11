3dcbbfa0c57309f37bfc8c5a8513d77861980987
Sadamori Kojaku <skojaku@iu.edu>
2022-11-11 07:04:56 -0500

    remove self-loop

--- belief_propagation/BP.py
+++ belief_propagation/BP.py
@@ -2,7 +2,7 @@
 # @Author: Sadamori Kojaku
 # @Date:   2022-11-02 13:34:28
 # @Last Modified by:   Sadamori Kojaku
-# @Last Modified time: 2022-11-04 00:13:33
+# @Last Modified time: 2022-11-11 06:39:03
 import os
 import pathlib
 import tempfile
@@ -12,21 +12,34 @@ from scipy import sparse
 import networkx as nx
 
 
-def detect(A, q):
+def detect(A, q, params_sbm="", mute=True):
+    A.setdiag(0)
+    A.eliminate_zeros()
+    A = sparse.csr_matrix(A)
     G = nx.from_scipy_sparse_array(A)
     for (n1, n2, d) in G.edges(data=True):
         d.clear()
+    for (n1, d) in G.nodes(data=True):
+        d.clear()
     with tempfile.TemporaryDirectory() as tmpdirname:
         root = pathlib.Path(__file__).parent.absolute()
+        # graph_file_name = f"./graph.gml"
+        # output_file_name = f"./tmp.csv"
         graph_file_name = f"{tmpdirname}/graph.gml"
         output_file_name = f"{tmpdirname}/tmp.csv"
+
         nx.write_gml(G, graph_file_name)
-        # os.chdir(tmpdirname)
+        if mute:
+            mute = ">/dev/null"
+        else:
+            mute = ""
         os.system(
-            # f"{root}/sbm learn -q {q} -l {graph_file_name} -w {output_file_name}"
-            f"{root}/sbm learn -q {q} -l {graph_file_name} -w {output_file_name} >/dev/null"
+            # f"{root}/sbm spc -q {q} -l {graph_file_name} -w {output_file_name} {params_sbm} >/dev/null"
+            f"{root}/sbm learn -q {q} -l {graph_file_name} -w {output_file_name} {params_sbm} {mute}"
+            # f"{root}/sbm learn -q {q} -l {graph_file_name} -w {output_file_name} {params_sbm} >/dev/null"
         )
-        cids = pd.read_csv(output_file_name)["block"].values
-        # os.chdir(root)
+        df = pd.read_csv(output_file_name)
+        cids = df["block"].values
         cids = np.unique(cids, return_inverse=True)[1]
-    return cids
+
+    return cids, df
--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: Sadamori Kojaku
+# @Date:   2022-11-04 00:14:13
+# @Last Modified by:   Sadamori Kojaku
+# @Last Modified time: 2022-11-11 06:38:11
+# %%
 import belief_propagation
 import networkx as nx
-import numpy as np 
+import numpy as np
+from scipy import sparse
 
 # Create a karate club network.
-A = nx.to_scipy_sparse_matrix(nx.karate_club_graph()) 
+A = nx.to_scipy_sparse_matrix(nx.karate_club_graph())
 
 # Detect communities
-# A: scipy sparse matrix. CSR format. 
+# A: scipy sparse matrix. CSR format.
 # q: Number of communities
-community_ids = belief_propagation.detect(A, q=3) 
+community_ids, df = belief_propagation.detect(A, q=2, params_sbm=f"-v0", mute=False)
+# community_ids = belief_propagation.detect(A, q=3)
 print(community_ids)
-np.savez("result.npz", com = community_ids)
+np.savez("result.npz", com=community_ids)
+
+# %%
