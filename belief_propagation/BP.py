8ed6c10f30d8486498797cca7a140f9bd07ff52e
Sadamori Kojaku <skojaku@iu.edu>
2022-12-11 21:25:10 -0500

    add parameters for initial guess

--- belief_propagation/BP.py
+++ belief_propagation/BP.py
@@ -2,7 +2,7 @@
 # @Author: Sadamori Kojaku
 # @Date:   2022-11-02 13:34:28
 # @Last Modified by:   Sadamori Kojaku
-# @Last Modified time: 2022-11-28 16:22:40
+# @Last Modified time: 2022-12-11 21:24:57
 import os
 import pathlib
 import tempfile
@@ -12,14 +12,16 @@ from scipy import sparse
 import networkx as nx
 
 
-def detect(A, q, iters=5, params_sbm="", mute=True):
+def detect(
+    A, q, iters=1, p_init=None, cab_init=None, ave_deg=None, params_sbm="", mute=True
+):
     """Belief propagation
 
     :param A: Adjacency matrix
     :type A: sparse.csr_matrix
     :param q: Number of communities
     :type q: int
-    :param iters: Number of communication detections to run, defaults to 5. The best communities in terms of the free energy will be returned.
+    :param iters: Number of communication detections to run, defaults to 1. The best communities in terms of the free energy will be returned.
     :type iters: int, optional
     :param params_sbm: parameters to be passed to the original "sbm" program, defaults to "". See the author's code for the details.
     :type params_sbm: str, optional
@@ -28,11 +30,26 @@ def detect(A, q, iters=5, params_sbm="", mute=True):
     :return: numpy.ndarray
     :rtype: an array of community memberships
     """
+
+    if ave_deg is None:
+        n_nodes = A.shape[0]
+        ave_deg = len(A.data) / n_nodes
+
+    if p_init is None:
+        p_init = np.ones(q) / q
+
+    if cab_init is None:
+        cab_init = np.ones((q, q)) + np.eye(q) * (q * ave_deg - 1)
+
+    params_sbm += " -p" + ",".join(["%f" % d for d in p_init])
+    params_sbm += " -c" + ",".join(["%d" % d for d in cab_init.reshape(-1)])
+
     A.setdiag(0)
     A.eliminate_zeros()
     A.data = A.data * 0 + 1
     A = sparse.csr_matrix(A)
     G = nx.from_scipy_sparse_array(A)
+
     for (n1, n2, d) in G.edges(data=True):
         d.clear()
     for (n1, d) in G.nodes(data=True):
@@ -52,7 +69,7 @@ def detect(A, q, iters=5, params_sbm="", mute=True):
             mute = ""
 
         energy = np.inf
-        cids= np.zeros(A.shape[0], dtype=int)
+        cids = np.zeros(A.shape[0], dtype=int)
         for _ in range(iters):
             os.system(
                 # f"{root}/sbm spc -q {q} -l {graph_file_name} -w {output_file_name} {params_sbm} >/dev/null"
