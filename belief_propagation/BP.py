481effe17fcfd928973caae3f996bd9cf6c67682
Sadamori Kojaku <skojaku@iu.edu>
2022-12-12 06:35:07 -0500

    make it work for long cab

--- belief_propagation/BP.py
+++ belief_propagation/BP.py
@@ -2,7 +2,7 @@
 # @Author: Sadamori Kojaku
 # @Date:   2022-11-02 13:34:28
 # @Last Modified by:   Sadamori Kojaku
-# @Last Modified time: 2022-12-11 21:28:12
+# @Last Modified time: 2022-12-12 06:34:45
 import os
 import pathlib
 import tempfile
@@ -13,7 +13,12 @@ import networkx as nx
 
 
 def detect(
-    A, q, iters=1, p_init=None, cab_init=None, ave_deg=None, params_sbm="", mute=True
+    A,
+    q,
+    iters=1,
+    init_memberships=None,
+    params_sbm="",
+    mute=True,
 ):
     """Belief propagation
 
@@ -23,10 +28,8 @@ def detect(
     :type q: int
     :param iters: Number of communication detections to run, defaults to 1. The best communities in terms of the free energy will be returned.
     :type iters: int, optional
-    :param p_init: p_init[i] is the fraction of the ith community. Set to uniform if not provided. Default to None.
-    :type p_init: numpy array or list.
-    :param cab_init: cab_init is the rescaled connectivity matrix, where cab[i,j] indicates the edge density x. Set to a diagonal matrix if not provided. Default to None.
-    :type cab_init: numpy array or list.
+    :param init_memberships: p_init and cab will be initialized by using the memberships.
+    :type init_memberships: numpy array or list.
     :param p_init: p_init[i] is the fraction of the ith community
     :type p_init: numpy array or list.
     :param params_sbm: parameters to be passed to the original "sbm" program, defaults to "". See the author's code for the details.
@@ -36,19 +39,19 @@ def detect(
     :return: numpy.ndarray
     :rtype: an array of community memberships
     """
-
-    if ave_deg is None:
+    if init_memberships is not None:
         n_nodes = A.shape[0]
-        ave_deg = len(A.data) / n_nodes
-
-    if p_init is None:
-        p_init = np.ones(q) / q
-
-    if cab_init is None:
-        cab_init = np.ones((q, q)) + np.eye(q) * (q * ave_deg - 1)
-
-    params_sbm += " -p" + ",".join(["%f" % d for d in p_init])
-    params_sbm += " -c" + ",".join(["%d" % d for d in cab_init.reshape(-1)])
+        l, memberships = np.unique(init_memberships, return_inverse=True)
+        K = len(l)
+        U = sparse.csr_matrix(
+            (np.ones_like(memberships), (np.arange(n_nodes), memberships)),
+            shape=(n_nodes, K),
+        )
+        Nc = np.array(U.sum(axis=0)).reshape(-1)
+        cab_init = np.maximum(n_nodes * ((U.T @ A @ U).toarray() / np.outer(Nc, Nc)), 1)
+        p_init = Nc / n_nodes
+    else:
+        cab_init, p_init = None, None
 
     A.setdiag(0)
     A.eliminate_zeros()
@@ -63,11 +66,16 @@ def detect(
     with tempfile.TemporaryDirectory() as tmpdirname:
         root = pathlib.Path(__file__).parent.absolute()
         # graph_file_name = f"./graph.gml"
+        # cab_file_name = f"./tmp.cab"
         # output_file_name = f"./tmp.csv"
         graph_file_name = f"{tmpdirname}/graph.gml"
+        cab_file_name = f"{tmpdirname}/tmp.cab"
         output_file_name = f"{tmpdirname}/tmp.csv"
 
         nx.write_gml(G, graph_file_name)
+        if cab_init is not None:
+            generate_cab_file(cab_file_name, p_init, cab_init)
+            params_sbm += f" -L {cab_file_name}"
 
         if mute:
             mute = ">/dev/null"
@@ -78,9 +86,7 @@ def detect(
         cids = np.zeros(A.shape[0], dtype=int)
         for _ in range(iters):
             os.system(
-                # f"{root}/sbm spc -q {q} -l {graph_file_name} -w {output_file_name} {params_sbm} >/dev/null"
                 f"{root}/sbm learn -q {q} -l {graph_file_name} -w {output_file_name} {params_sbm} {mute}"
-                # f"{root}/sbm learn -q {q} -l {graph_file_name} -w {output_file_name} {params_sbm} >/dev/null"
             )
             df = pd.read_csv(output_file_name)
             energy_t = df["energy"].values[0]
@@ -90,3 +96,18 @@ def detect(
                 cids = np.unique(cids, return_inverse=True)[1]
 
     return cids
+
+
+def generate_cab_file(filename, p, cab):
+    pstr = " ".join([f"{d:.4f}" for d in p])
+    cabstr = "\n".join(
+        [
+            " ".join([f"{cab[r,c]:.4f}" for c in range(cab.shape[1])])
+            for r in range(cab.shape[0])
+        ]
+    )
+    s = f"#Vector_na:\n{pstr}\n#Matrix_cab:\n{cabstr}"
+    text_file = open(filename, "w")
+    text_file.write(s)
+    text_file.close()
+    return
